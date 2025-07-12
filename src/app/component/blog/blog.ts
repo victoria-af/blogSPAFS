b20387ec00c126996f3a996b78bf9932ea5f9446
Victoria AF <fafascinada@gmail.com>
2025-07-12 19:54:55 +0200

    Creado array, interfaz y noticias, inicializado

--- src/app/component/blog/blog.ts
+++ src/app/component/blog/blog.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { INews } from '../../interfaces/inews.interface';
+
 
 @Component({
   selector: 'app-blog',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './blog.css'
 })
 export class Blog {
+newsList: INews[] = [
+  {
+    title: 'Titulo noticia 1',
+    imageUrl: "https://placehold.co/600x300",
+    text: 'texto de la noticia 1',
+    date: '2025-07-01'
+  },
+  {
+    title: 'Titulo noticia 2',
+    imageUrl: "https://placehold.co/600x300",
+    text: 'texto de la noticia 2',
+    date: '2025-07-05'
+  }
+];
 
 }
---
+++ src/app/interfaces/inews.interface.ts
@@ -0,0 +1,6 @@
+export interface INews {
+    title: string;
+    imageUrl: string;
+    text: string;
+    date: string;
+}
